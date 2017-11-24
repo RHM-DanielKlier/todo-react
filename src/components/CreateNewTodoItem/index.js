2fcc036cef006f813e561250a1b6c9b14f8849b8
Daniel Klier <klier@redhotmagma.de>
2017-11-24 13:27:11 +0100

    Enter todo title on create

--- src/components/CreateNewTodoItem/index.js
+++ src/components/CreateNewTodoItem/index.js
@@ -1,25 +1,40 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './Styles.css';
 
-function CreateNewTodoItem(props) {
-    return (
-        <form onSubmit={(event) => {
-            props.onCreateNewItem(event.target.values);
-            console.log(event, event.target.values);
-            event.preventDefault();
-        }}>
-            <input type="text" placeholder="Enter ToDo title here"/>
-            <button type="submit">Create New</button>
-        </form>
-    )
+class CreateNewTodoItem extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            title: ''
+        };
+    }
+
+    render() {
+        return (
+            <form className="create-new-todo" onSubmit={(event) => {
+                this.props.onCreateNewItem(this.state.title);
+                event.preventDefault();
+            }}>
+                <input type="text" placeholder="Enter ToDo title here" value={this.state.title}
+                       onChange={e => this.setState({title: e.target.value})}
+                />
+                <button type="submit">Create New</button>
+            </form>
+        );
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    static propTypes = {
+        onCreateNewItem: PropTypes.func
+    };
+
+    // noinspection JSUnusedGlobalSymbols
+    static defaultProps = {
+        onCreateNewItem: a => a
+    };
 }
-
-CreateNewTodoItem.propTypes = {
-    onCreateNewItem: PropTypes.func
-};
-
-CreateNewTodoItem.defaultProps = {
-    onCreateNewItem: a => a
-};
 
 export default CreateNewTodoItem;
